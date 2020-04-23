a1be55f1be39b8b2d487be3900f3a856aa015b54
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2020-04-23 09:46:04 +0200

    Modified plugin name.

--- jamp.php
+++ jamp.php
@@ -12,7 +12,7 @@
  * @package           Jamp
  *
  * @wordpress-plugin
- * Plugin Name:       Just Another Memo Plugin (JAMP)
+ * Plugin Name:       JAMP (Just Another Memo Plugin)
  * Plugin URI:        https://www.andreaporotti.it
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
