3d399468b6d38316682167ef7649424dab584660
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2020-05-13 16:23:34 +0200

    The meta box fields are disabled when creating a note with settings coming from the create link.

--- admin/partials/jamp-admin-meta-box.php
+++ admin/partials/jamp-admin-meta-box.php
@@ -13,7 +13,8 @@
 // Add nonce field.
 wp_nonce_field( 'jamp_meta_box_nonce_secret_action', 'jamp-meta-box-nonce' );
 
-$screen = get_current_screen();
+$screen           = get_current_screen();
+$is_form_disabled = false;
 
 // Build array for the meta box fields.
 if ( 'add' === $screen->action ) { // Creating a new note.
@@ -24,11 +25,19 @@ if ( 'add' === $screen->action ) { // Creating a new note.
 	parse_str( $querystring, $params );
 
 	$jamp_meta                     = array();
-	$jamp_meta['jamp_scope']       = array( ( isset( $params['jamp_scope'] ) ) ? $params['jamp_scope'] : '' );
+	$jamp_meta['jamp_scope']       = array( ( isset( $params['jamp_scope'] ) )       ? $params['jamp_scope']       : '' );
 	$jamp_meta['jamp_target_type'] = array( ( isset( $params['jamp_target_type'] ) ) ? $params['jamp_target_type'] : '' );
-	$jamp_meta['jamp_target']      = array( ( isset( $params['jamp_target'] ) ) ? $params['jamp_target'] : '' );
+	$jamp_meta['jamp_target']      = array( ( isset( $params['jamp_target'] ) )      ? $params['jamp_target']      : '' );
 
-	// The scope must be set. If it's empty or wrong, let's set it to "global".
+	// The form is disabled when creating an already configured note (if at least one setting exists).
+	foreach ($jamp_meta as $key => $value) {
+		if ( ! empty( $value[0] ) ) {
+			$is_form_disabled = true;
+			break;
+		}
+	}
+
+	// At least the scope must be set. If it's empty or wrong, let's set it to "global".
 	if ( ! in_array( $jamp_meta['jamp_scope'][0], array( 'global', 'section', 'entity' ), true ) ) {
 		$jamp_meta['jamp_scope'] = array( 'global' );
 	}
@@ -42,58 +51,68 @@ if ( 'add' === $screen->action ) { // Creating a new note.
 
 <input type="hidden" id="saved-target" value="<?php echo ( isset( $jamp_meta['jamp_target'] ) ) ? esc_attr( $jamp_meta['jamp_target'][0] ) : ''; ?>">
 
-<div class="meta-field meta-scope no-margin-top">
-	<span><?php esc_html_e( 'Select the Note scope.', 'jamp' ); ?></span>
-	<br>
-	<label for="scope-global">
-		<input type="radio" name="scope" id="scope-global" value="global" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'global' ) : ''; ?>>
-		<?php esc_html_e( 'Global', 'jamp' ); ?>
-	</label>
-	<br>
-	<label for="scope-section">
-		<input type="radio" name="scope" id="scope-section" value="section" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'section' ) : ''; ?>>
-		<?php esc_html_e( 'Section', 'jamp' ); ?>
-	</label>
-	<br>
-	<label for="scope-entity">
-		<input type="radio" name="scope" id="scope-entity" value="entity" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'entity' ) : ''; ?>>
-		<?php esc_html_e( 'Item', 'jamp' ); ?>
-	</label>
-</div>
+<?php // If form is disabled, create hidden fields in place of the actual fields. ?>
+<?php if ( $is_form_disabled ) : ?>
+	<input type="hidden" name="scope" id="scope" value="<?php echo esc_attr( $jamp_meta['jamp_scope'][0] ); ?>">
+	<input type="hidden" name="section" id="section" value="<?php echo esc_attr( $jamp_meta['jamp_target'][0] ); ?>">
+	<input type="hidden" name="target-type" id="target-type" value="<?php echo esc_attr( $jamp_meta['jamp_target_type'][0] ); ?>">
+	<input type="hidden" name="target" id="target" value="<?php echo esc_attr( $jamp_meta['jamp_target'][0] ); ?>">
+<?php endif; ?>
 
-<div class="meta-field meta-section">
-	<label for="section" class="display-block"><?php esc_html_e( 'Select the Section.', 'jamp' ); ?></label>
-	<select name="section" id="section">
-		<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
+<fieldset <?php disabled( $is_form_disabled, true ) ?>>
+	<div class="meta-field meta-scope no-margin-top">
+		<span><?php esc_html_e( 'Select the Note scope.', 'jamp' ); ?></span>
+		<br>
+		<label for="scope-global">
+			<input type="radio" name="scope" id="scope-global" value="global" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'global' ) : ''; ?>>
+			<?php esc_html_e( 'Global', 'jamp' ); ?>
+		</label>
+		<br>
+		<label for="scope-section">
+			<input type="radio" name="scope" id="scope-section" value="section" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'section' ) : ''; ?>>
+			<?php esc_html_e( 'Section', 'jamp' ); ?>
+		</label>
+		<br>
+		<label for="scope-entity">
+			<input type="radio" name="scope" id="scope-entity" value="entity" <?php ( isset( $jamp_meta['jamp_scope'] ) ) ? checked( $jamp_meta['jamp_scope'][0], 'entity' ) : ''; ?>>
+			<?php esc_html_e( 'Item', 'jamp' ); ?>
+		</label>
+	</div>
 
-		<?php foreach ( $args['args']['sections'] as $section ) : ?>
+	<div class="meta-field meta-section">
+		<label for="section" class="display-block"><?php esc_html_e( 'Select the Section.', 'jamp' ); ?></label>
+		<select name="section" id="section">
+			<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
 
-			<option value="<?php echo esc_attr( $section['url'] ); ?>" <?php ( isset( $jamp_meta['jamp_target'] ) ) ? selected( $jamp_meta['jamp_target'][0], $section['url'] ) : ''; ?> <?php echo( ! $section['is_submenu'] ) ? 'disabled' : ''; ?>>
-				<?php echo esc_html( $section['name'] ); ?>
-			</option>
+			<?php foreach ( $args['args']['sections'] as $section ) : ?>
 
-		<?php endforeach; ?>
-	</select>
-</div>
+				<option value="<?php echo esc_attr( $section['url'] ); ?>" <?php ( isset( $jamp_meta['jamp_target'] ) ) ? selected( $jamp_meta['jamp_target'][0], $section['url'] ) : ''; ?> <?php echo( ! $section['is_submenu'] ) ? 'disabled' : ''; ?>>
+					<?php echo esc_html( $section['name'] ); ?>
+				</option>
 
-<div class="meta-field meta-target-type">
-	<label for="target-type" class="display-block"><?php esc_html_e( 'Select the item type.', 'jamp' ); ?></label>
-	<select name="target-type" id="target-type">
-		<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
+			<?php endforeach; ?>
+		</select>
+	</div>
 
-		<?php foreach ( $args['args']['target_types'] as $target_type ) : ?>
+	<div class="meta-field meta-target-type">
+		<label for="target-type" class="display-block"><?php esc_html_e( 'Select the item type.', 'jamp' ); ?></label>
+		<select name="target-type" id="target-type">
+			<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
 
-			<option value="<?php echo esc_attr( $target_type['name'] ); ?>" <?php ( isset( $jamp_meta['jamp_target_type'] ) ) ? selected( $jamp_meta['jamp_target_type'][0], $target_type['name'] ) : ''; ?>>
-				<?php echo esc_html( $target_type['label'] ); ?>
-			</option>
+			<?php foreach ( $args['args']['target_types'] as $target_type ) : ?>
 
-		<?php endforeach; ?>
-	</select>
-</div>
+				<option value="<?php echo esc_attr( $target_type['name'] ); ?>" <?php ( isset( $jamp_meta['jamp_target_type'] ) ) ? selected( $jamp_meta['jamp_target_type'][0], $target_type['name'] ) : ''; ?>>
+					<?php echo esc_html( $target_type['label'] ); ?>
+				</option>
 
-<div class="meta-field meta-target">
-	<label for="target" class="display-block"><?php esc_html_e( 'Select the item.', 'jamp' ); ?></label>
-	<select name="target" id="target">
-		<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
-	</select>
-</div>
+			<?php endforeach; ?>
+		</select>
+	</div>
+
+	<div class="meta-field meta-target">
+		<label for="target" class="display-block"><?php esc_html_e( 'Select the item.', 'jamp' ); ?></label>
+		<select name="target" id="target">
+			<option value=""><?php esc_html_e( 'select...', 'jamp' ); ?></option>
+		</select>
+	</div>
+</fieldset>
