303d6e241483b629d36c9cc902c622127ef68c76
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2020-07-11 17:40:37 +0200

    Added a placeholder when a note in the admin bar has no title.

--- admin/partials/jamp-admin-admin-bar.php
+++ admin/partials/jamp-admin-admin-bar.php
@@ -36,9 +36,10 @@ if ( ! empty( $global_notes ) ) {
 		$note_author        = get_userdata( $note->post_author );
 		$note_modified_date = date_i18n( $date_time_format, strtotime( $note->post_modified ) );
 		$note_created_date  = date_i18n( $date_time_format, strtotime( $note->post_date ) );
+		$note_title         = ( ! empty( $note->post_title ) ? $note->post_title : __( '(no title)' ) );
 
 		$html .= '<div class="jamp-admin-bar-note" data-note="' . esc_attr( $note->ID ) . '" data-scope="global">'
-				. '<span class="jamp-admin-bar-note__title">' . esc_html( $note->post_title ) . '</span>'
+				. '<span class="jamp-admin-bar-note__title">' . esc_html( $note_title ) . '</span>'
 				. '<span class="jamp-admin-bar-note__actions">'
 				. '<a class="jamp-admin-bar-action jamp-admin-bar-action--info" href="#" title="' . esc_html__( 'Details', 'jamp' ) . '"></a>'
 				. '<a class="jamp-admin-bar-action jamp-admin-bar-action--edit" href="' . esc_url( get_edit_post_link( $note->ID ) ) . '" title="' . esc_html__( 'Edit', 'jamp' ) . '"></a>'
