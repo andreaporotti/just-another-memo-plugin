1c5ff71d61dfe9e07b675279d8dcdd5c9fe789ef
Andrea Porotti <53380085+andreaporotti@users.noreply.github.com>
2022-12-04 15:43:43 +0100

    Show color in users column notes.

--- admin/partials/jamp-admin-column-users.php
+++ admin/partials/jamp-admin-column-users.php
@@ -46,7 +46,13 @@ if ( 'jamp_note' === $column_name ) {
 			$note_created_date  = date_i18n( $date_time_format, strtotime( $note->post_date ) );
 			$note_title         = ( ! empty( $note->post_title ) ? $note->post_title : __( '(no title)' ) );
 
-			$column_content .= '<div class="jamp-column-note" data-note="' . esc_attr( $note->ID ) . '">'
+			$jamp_meta        = get_post_meta( $note->ID );
+			$note_color_class = '';
+			if ( isset( $jamp_meta['jamp_color'][0] ) && ! empty( $jamp_meta['jamp_color'][0] ) ) {
+				$note_color_class = 'jamp-column-note--color-' . $jamp_meta['jamp_color'][0];
+			}
+
+			$column_content .= '<div class="jamp-column-note ' . esc_attr( $note_color_class ) . '" data-note="' . esc_attr( $note->ID ) . '">'
 								. '<span class="jamp-column-note__title">' . esc_html( $note_title ) . '</span>'
 								. '<div class="jamp-column-note__container">'
 									. '<div class="jamp-column-note__content">' . wp_kses_post( $note->post_content ) . '</div>'
